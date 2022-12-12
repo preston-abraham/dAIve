e347ec128b2c02de84449297c8ba06a6963ea11c
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2022-12-12 17:11:24 -0600

    dAIve v1.1
    New Features: Content Moderation, more finely tuned model for better responses, logging of questions and answers for further training

--- dAIve.py
+++ dAIve.py
@@ -5,7 +5,9 @@ import streamlit as st
 openai.organization = "org-eptWwJzwl8LLZVNyAH1xBxbF"
 openai.api_key = st.secrets['api_key']
 
-st.title('dAIve')
+st.title('dAIve v1.1')
+
+st.markdown('***New Features**: Content Moderation, More finely-tuned model for better responses,Logging of questions and answers for further training*')
 
 from PIL import Image
 image = Image.open('dAIve.png')
@@ -36,11 +38,38 @@ if mode == 'Evil Dave':
     prefix = 'Respond to the following prompt like an evil version person who beleives the exact opposite of what Dave Ramsey thinks and wants me to make bad financial decision would respond, with some sarcasm and slight rudeness: '
 
 if st.button('Get answer'): 
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      prompt = prefix + q,
-      temperature = 0.15,
-      top_p = 1,
-      max_tokens = 500
+    if st.button('Get answer'): 
+    
+    # Check if content is flaggable
+    
+    content_to_classify = q
+
+    response_flag = openai.Completion.create(
+      model="content-filter-alpha",
+      prompt = "<|endoftext|>"+content_to_classify+"\n--\nLabel:",
+      temperature=0,
+      max_tokens=1,
+      top_p=0,
+      logprobs=10
     )
-    st.markdown(response["choices"][0]["text"])
+    
+    output_label = response_flag["choices"][0]["text"]
+    
+    if str(output_label) == '0':
+        
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt = prefix + q,
+          temperature = 0.15,
+          top_p = 1,
+          max_tokens = 500
+        )
+
+        a = response["choices"][0]["text"]
+
+        st.markdown(a)
+        print('['+mode+':'+q + ',' + a+']')
+    else:
+        st.markdown("""
+        Hey there, I'm dAIve and I'm here to help you with your financial questions. However, I'm sorry but I'm not able to answer that question for you. It goes against my programming to provide responses that may be considered offensive or inappropriate. I'm here to help you make smart financial decisions, so if you have any other questions, I'm here to assist you within the parameters of my abilities.
+""")
